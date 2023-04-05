dd5a8ec5987d86e925983aebe76d58bc4c6b5ea1
David Riggins <driggins@esri.com>
2023-04-04 17:04:08 -0700

    Get request.

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 // GET REQUEST
 function getTodos() {
-    console.log("GET Request");
+    // axios({
+    //     method: "get",
+    //     url: "https://jsonplaceholder.typicode.com/todos",
+    //     params: {
+    //         _limit: 10,
+    //     },
+    // })
+    //     .then((res) => showOutput(res))
+    //     .catch((err) => console.error(err));
+
+    // axios
+    //     .get("https://jsonplaceholder.typicode.com/todos", {
+    //         params: { _limit: 10 },
+    //     })
+    //     .then((res) => showOutput(res))
+    //     .catch((err) => console.error(err));
+
+    axios
+        .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+        .then((res) => showOutput(res))
+        .catch((err) => console.error(err));
 }
 
 // POST REQUEST
