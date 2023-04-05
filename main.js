e96d2581980858a04ef9a5a95b1813d6f6d51163
David Riggins <driggins@esri.com>
2023-04-04 17:46:20 -0700

    Custom headers.

--- main.js
+++ main.js
@@ -104,7 +104,24 @@ function getData() {
 
 // CUSTOM HEADERS
 function customHeaders() {
-    console.log("Custom Headers");
+    const config = {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "sometoken",
+        },
+    };
+
+    axios
+        .post(
+            "https://jsonplaceholder.typicode.com/todos",
+            {
+                title: "New Todo",
+                completed: false,
+            },
+            config
+        )
+        .then((res) => showOutput(res))
+        .catch((err) => console.error(err));
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
